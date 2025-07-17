4ab058841f7e9a6e476fc5ab6539e8d745131da6
devanaclimgo <anaclimgo@gmail.com>
2025-07-17 18:10:15 -0300

    feat: spotlight component added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
-      <div></div>
+      <div>
+        <Spotlight />
+      </div>
     </div>
   )
 }
