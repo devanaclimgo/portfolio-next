4a252c54f5cb8bb1181c05b88e9b81884aa5ad5a
devanaclimgo <anaclimgo@gmail.com>
2025-07-17 18:34:14 -0300

    feat: added full style of hero section, titles and buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+
+import { FaLocationArrow } from "react-icons/fa6";
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './MagicButton'
 
 const Hero = () => {
   return (
@@ -24,6 +28,32 @@ const Hero = () => {
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
        bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
+
+       <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Ana, a Rails/React Developer based in Brazil.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+
     </div>
   )
 }
