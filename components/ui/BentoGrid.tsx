a0864a0cb3df180c8a90a7bb792f297e5f5e25b7
devanaclimgo <anaclimgo@gmail.com>
2025-07-25 18:11:35 -0300

    fix:  added "use client" and updated the lottie react import

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 
 import { cn } from "@/lib/utils";
 
