1508317c85db0d5ad2180e6841a20af56b6e984f
devanaclimgo <anaclimgo@gmail.com>
2025-08-15 18:20:10 -0300

    feat: added approach component import to page view

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects"
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
