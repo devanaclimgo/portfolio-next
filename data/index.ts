f75b5bdb3ee8c98dbc20abc737ba827451f5ba9d
devanaclimgo <anaclimgo@gmail.com>
2025-08-20 18:29:34 -0300

    chore: edited work experience 2

--- data/index.ts
+++ data/index.ts
@@ -174,8 +174,8 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Frontend Dev | Pipoca Ágil",
+    desc: "Developed React/Tailwind components, Dockerized workflows, integrated Node.js APIs, and mentored junior devs in Agile teams.",
     className: "md:col-span-2",
     thumbnail: "/exp2.svg",
   },
